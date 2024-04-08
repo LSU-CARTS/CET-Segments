000117ab3e50cb343dfe9efd867caa4c0058f29c
sburto <sburto@umich.edu>
2024-04-08 11:16:25 -0400

    cost benefit calculations
    should fit pretty cleanly into existing code

---
+++ costbenefit.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 10:08:29 2024
+
+@author: sburt
+"""
+
+import numpy as np
+
+#Placeholder values for testing
+cmf = 0.825
+ExistingCrashes = [0.09, 0.07, 0.54, 2.75, 5.54]
+CrashCosts = [1710561.00, 489446.00, 173578.00, 58636.00, 24982.00]
+y = 20
+Inflation = 0.04
+EstimatedCost = 240960
+
+
+CrashReduction = ExistingCrashes - np.multiply(cmf,ExistingCrashes)
+BenefitsPerYear = np.dot(CrashReduction,CrashCosts)
+
+# CostsPerYear = np.dot(ExistingCrashes,CrashCosts)
+
+def pv(r: float, n: int, pmt):
+    # takes in rate (r), number of periods (n), and payment size (pmt)
+    # future value (f) is not needed and therefore will always be 0
+    # 'when' (or: when payments are made, beginning or end of period (w)) is not needed, will always be 0
+    f = 0
+    w = 0
+    present_v = ((r + 1) ** (-n)*(-f * r - pmt * ((r + 1)**n - 1) * (r*w + 1)))/r
+    return present_v
+
+SL_Benefit = -pv(Inflation, y, BenefitsPerYear)
+BenCost = SL_Benefit/EstimatedCost
--- crash_type_percentage.py
+++ crash_type_percentage.py
@@ -18,37 +18,34 @@ for k in ctype_expansion.keys():
         if ctype_expansion[k][n] not in Unique_Ctypes:
             Unique_Ctypes.append(ctype_expansion[k][n])
             
-Coll_CDs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'Z']
-Sev_CDs = ['A', 'B', 'C', 'D', 'E']
-
 # ctype_expansion.to_csv('ctypes.csv')
 
-CrashData = pd.read_excel('crash1 I-10 BR Bridge 17-21.xlsx')
+CrashData = pd.read_excel('077-05_17-19.xlsx')
 
-y = max(CrashData['CRASH_YEAR']) - min(CrashData['CRASH_YEAR']) + 1
+Coll_CDs = [0, 100, 101, 102, 103, 105, 200, 300, 501, 502, 505, 980]
+Coll_types = ['NOT A COLLISION BETWEEN TWO MOTOR VEHICLES', 'ANGLE - LEFT OVERTAKE', 'ANGLE - LEFT ACROSS FLOW',
+              'ANGLE - RIGHT INTO FLOW', 'ANGLE - LEFT INTO FLOW', 'ANGLE - PERPENDICULAR/OTHER ANGLE',
+              ]
+Sev_CDs = [100, 101, 102, 103, 104]
 
-IntCrashData = CrashData[CrashData['INTERSECTION'] == 1]
-SegCrashData = CrashData[CrashData['INTERSECTION'] == 0]
+y = max(CrashData['CrashDate']).year - min(CrashData['CrashDate']).year + 1
 
-TC_Int = len(IntCrashData['INTERSECTION'])
-TC_Seg = len(SegCrashData['INTERSECTION'])
+TC = len(CrashData['CrashDate'])
 
-Int_pvt = pd.pivot_table(IntCrashData[['MAN_COLL_CD','SEVERITY_CD']], index='MAN_COLL_CD', columns='SEVERITY_CD',
+Pvt = pd.pivot_table(CrashData[['MannerCollision','CrashSeverityCode']], index='MannerCollision', columns='CrashSeverityCode',
                      aggfunc=len, fill_value=0, margins=True, dropna=False)
-Seg_pvt = pd.pivot_table(SegCrashData[['MAN_COLL_CD','SEVERITY_CD']], index='MAN_COLL_CD', columns='SEVERITY_CD',
-                     aggfunc=len, fill_value=0, margins=True, dropna=False)
-
 
 for n in range(len(Coll_CDs)):
-    if Coll_CDs[n] not in IntCrashData['MAN_COLL_CD'].unique():
-        df1 = Int_pvt[:n]
-        df1.loc[Coll_CDs[n]] = np.zeros(len(Int_pvt.keys()))
-        df2 = Int_pvt[n:]
-        Int_pvt = pd.concat([df1,df2])
+    if Coll_CDs[n] not in CrashData['MannerCollisionCode'].unique():
+        df1 = Pvt[:n]
+        df1.loc[Coll_CDs[n]] = np.zeros(len(Pvt.keys()))
+        df2 = Pvt[n:]
+        Pvt = pd.concat([df1,df2])
 for k in range(len(Sev_CDs)):
-    if Sev_CDs[k] not in Int_pvt.keys():
-        Int_pvt.insert(k, Sev_CDs[k], 0)
+    if Sev_CDs[k] not in Pvt.keys():
+        Pvt.insert(k, Sev_CDs[k], 0)
 
-Int_pct = Int_pvt/TC_Int
-Seg_pct = Seg_pvt/TC_Seg
+Pct = Pvt/TC
 
+CType_Pvt = pd.pivot_table(CrashData[['Crash_Type', 'CrashSeverityCode']], index='Crash_Type' columns='CrashSeverityCode',
+                     aggfunc=len, fill_value=0, margins=True, dropna=False))
